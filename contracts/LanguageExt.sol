32e2b6d846cc53e89bd55d89a5359ac23f81aa2b
collins ihezie <collinsihezie6@gmail.com>
2023-05-16 16:11:44 +0100

    learned advanced solidity concepts

--- contracts/LanguageExt.sol
+++ contracts/LanguageExt.sol
@@ -51,8 +51,15 @@ contract LanguageExt{
 //     string model;
 //     uint value;
 // }
+// Car car = Car({make: "Make", model: "Model", value: 25});
 //Enums
-
+// enum States {
+//     PENDING,
+//     LOADING,
+//     SUCCESS,
+//     FAILURE
+// }
+// States state = States.LOADING;
 
 //Understanding Memory and storage
 contract Numbers{
@@ -93,3 +100,208 @@ contract Numbers{
        array[0] = 1;
     }
 }
+
+contract LearnEvents {
+    /*
+    Events allow contracts to perform logging on the Ethereum blockchain. 
+    Logs for a given contract can be parsed later to perform updates on the 
+    frontend interface, for example. They are commonly used to allow frontend 
+    interfaces to listen for specific events and update the user interface, 
+    or used as a cheap form of storage.
+    */
+
+    event TestCalled(address sender, string message);
+
+    function test() public {
+        emit TestCalled(msg.sender, "Someone called test()");
+    }
+}
+
+/*
+Inheritance
+Inheritance is the procedure by which one contract can inherit the attributes and methods of another contract. Solidity supports multiple inheritance. Contracts can inherit other contract by using the is keyword.
+
+Note: We actually also did Inheritance in the Freshman Track Cryptocurrency and NFT tutorials - where we inherited from the ERC20 and ERC721 contracts respectively.
+
+A parent contract which has a function that can be overridden by a child contract must be declared as a virtual function.
+
+A child contract that is going to override a parent function must use the override keyword.
+
+The order of inheritance matters if parent contracts share methods or attributes by the same name.
+*/
+
+contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    // Override A.foo();
+    // But also allow this function to be overridden by further children
+    // So we specify both keywords - virtual and override
+    function foo() public pure virtual override returns (string memory) {
+        return "B";
+    }
+}
+
+contract C is A {
+    // Similar to contract B above
+    function foo() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+// When inheriting from multiple contracts,
+// if a function is defined multiple times, the right-most 
+// parent contract's function is used.
+contract D is B, C {
+    // D.foo() returns "C"
+    // since C is the right-most parent with function foo();
+    // override (B,C) means we want to override a method that exists in two parents
+    function foo() public pure override (B, C) returns (string memory) {
+        // super is a special keyword that is used to call functions
+        // in the parent contract
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    // E.foo() returns "B"
+    // since B is the right-most parent with function foo();
+    function foo() public pure override (C, B) returns (string memory) {
+        return super.foo();
+    }
+}
+
+/*
+Currently, the recommended way to transfer ETH from a 
+contract is to use the call function. The call function 
+returns a bool indicating success or failure of the transfer.
+ */
+
+contract SendEther {
+    function sendEth(address payable _to) public payable {
+        // Just forward the ETH received in this payable function
+        // to the given address
+        uint amountToSend = msg.value;
+        // call returns a bool value specifying success or failure
+        (bool success, bytes memory data) = _to.call{value: msg.value}("");
+        require(success == true, "Failed to send ETH");
+    }
+}
+
+/*
+However, if you are writing a contract that you want to be able 
+to receive ETH transfers directly, you must have at least one 
+of the functions below
+
+receive() external payable
+fallback() external payable
+receive() is called if msg.data is an empty value, and fallback() is used otherwise.
+*/
+
+contract ReceiveEther {
+     /*
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
+    // get contract balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+
+//Interfaces
+//  Interfaces in Solidity behave like header files, 
+// and serve similar purposes to the ABI we have been
+// using when calling contracts from the frontend.
+
+interface MinimalERC20 {
+    // Just include the functions we are interested in
+    // in the interface
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract MyInterfaceContract {
+    MinimalERC20 _minimalErc20;
+
+    constructor() {
+      // Initialize a MinimalERC20 contract instance
+      _minimalErc20 = MinimalERC20(msg.sender);
+    }
+
+    function mustHaveSomeBalance() public view {
+        // Require that the caller of this transaction has a non-zero
+        // balance of tokens in the external ERC20 contract
+        uint balance = _minimalErc20.balanceOf(msg.sender);
+        require(balance > 0, "You don't own any tokens of external contract");
+    }
+}
+
+// Solidity Libraries
+/*
+Libraries are similar to contracts in Solidity, 
+with a few limitations. Libraries cannot contain 
+any state variables, and cannot transfer ETH.
+
+Typically, libraries are used to add helper functions 
+to your contracts. An extremely commonly used library 
+in Solidity world is SafeMath - which ensures that 
+mathematical operations do not cause an integer underflow or overflow.
+*/
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint) {
+        uint z = x + y;
+        // If z overflowed, throw an error
+        require(z >= x, "uint overflow");
+        return z;
+    }
+}
+
+contract TestSafeMath {
+    function testAdd(uint x, uint y) public pure returns(uint) {
+        return SafeMath.add(x, y);
+    }
+}
+
+// Provider
+/*
+A Provider is an Ethereum node connection that allows you 
+to read data from its state. You will use a Provider to 
+do things like calling read-only functions in smart contracts, 
+fetching balances of accounts, fetching transaction details, etc.
+*/
+
+// Signer
+/*
+A Signer is an Ethereum node connection that allows you 
+to write data to the blockchain. You will use a Signer 
+to do things like calling write functions in smart contracts,
+transferring ETH between accounts, etc. To do so, 
+the Signer needs access to a Private Key it can use 
+for making transactions on behalf of an account.
+*/
+// Additionally, a Signer can do everything a Provider can.
+// You can do both, reading and writing, using a Signer,
+// but a Provider is only good for reading data.
